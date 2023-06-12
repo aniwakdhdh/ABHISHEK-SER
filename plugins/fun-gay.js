c111eb3fd6fb5e0cbce0c5e2bb8020890bccf150
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-06-12 20:20:44 +0530

    ABHISHEK-SER💫👻

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,9 +1,11 @@
-let handler = async (m, { conn }) => {
+
+  let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let name = conn.getName(who)
-  conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/overlay/gay', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png'), 
-  }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nWho wants to rape this gay☠️? `, m)
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Abhi.jpg')
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/gay', {
+    avatar: pp, 
+  }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nWho Wants To Rape This Gay☠️? `, m)
 }
 
 handler.help = ['gay @user']
