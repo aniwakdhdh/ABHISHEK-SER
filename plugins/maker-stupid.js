56f170e9a04205e48f5d4c2cab2b4935755971f7
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-06-12 22:08:13 +0530

    Update maker-stupid.js

--- plugins/maker-stupid.js
+++ plugins/maker-stupid.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/its-so-stupid', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    dog: text || 'im+stupid'
+    dog: text || 'Im Stupid'
     }), 'error.png', `*@${author}*`, m)
     }  
     handler.help = ['itssostupid', 'iss', 'stupid']
