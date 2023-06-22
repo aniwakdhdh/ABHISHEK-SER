25044a62f4bd54175eb9c01e224090155aee04e6
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-06-22 08:33:23 +0530

    Create maker-quote.js

---
+++ plugins/maker-quote.js
@@ -0,0 +1,80 @@
+import fetch from 'node-fetch';
+import { support, sticker } from '../lib/sticker.js';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+
+let handler = async (m, { conn, text }) => {
+  let userPfp = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png'; // use this as the default profile picture
+
+  try {
+    if (!text && !m.quoted) {
+      m.react('❔');
+      return m.reply(`Please provide a text (Type or mention a message)!`);
+    }
+
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    if (!(who in global.db.data.users)) throw '✳️ The user is not found in my database';
+
+    let user = global.db.data.users[who];
+    let { name } = global.db.data.users[who];
+
+    m.react(rwait);
+    let quoteText = m.quoted ? m.quoted.msg : text ? text : "";
+
+    let quoteJson = {
+      type: "quote",
+      format: "png",
+      backgroundColor: "#FFFFFF",
+      width: 1800,
+      height: 200, // Adjust the height value as desired
+      scale: 2,
+      messages: [
+        {
+          entities: [],
+          avatar: true,
+          from: {
+            id: 1,
+            name: name,
+            photo: {
+              url: userPfp,
+            },
+          },
+          text: quoteText,
+          replyMessage: {},
+        },
+      ],
+    };
+
+    let res = await fetch('https://bot.lyo.su/quote/generate', {
+      method: 'POST',
+      body: JSON.stringify(quoteJson),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    let json = await res.json();
+    let bufferImage = Buffer.from(json.result.image, 'base64');
+
+    // Save the bufferImage to a file
+    let tempImagePath = path.join(os.tmpdir(), 'tempImage.png');
+    fs.writeFileSync(tempImagePath, bufferImage);
+
+    // Send the image as a file
+    await conn.sendFile(m.chat, tempImagePath, 'quote.png', 'Here is the quote image:', m);
+
+    // Delete the temp file after using
+    fs.unlinkSync(tempImagePath);
+
+    m.react("🤡");
+
+  } catch (e) {
+    console.error(e);
+    m.react("😭");
+  }
+};
+
+handler.help = ['quote'];
+handler.tags = ['fun'];
+handler.command = ['quote'];
+
+export default handler;
