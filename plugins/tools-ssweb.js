2a9a1c313f015b1c8c3bc6b2a3a634f62609866e
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-06-29 21:53:46 +0530

    Update tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, command, args, text }) => {
     let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
     let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
     //let ss = await fg.ssweb(url, /f$/i.test(command), args[1])
-    conn.sendFile(m.chat, ss, 'ssweb.png', '_✅Made By ABHISHEK-SER_', m) 
+    conn.sendFile(m.chat, ss, 'ssweb.png', '_Made By ABHISHEK-SER✅_', m) 
    m.react(done) 
 }
 handler.help = ['ssweb <url>']
