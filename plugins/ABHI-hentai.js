2ccdbbb11cbecdd66bceda840d9f5d1d9599cf6c
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-27 23:42:35 +0530

    Update ABHI-hentai.js

--- plugins/ABHI-hentai.js
+++ plugins/ABHI-hentai.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
-    if (!global.db.data.chats[m.chat].nsfw) throw `🚫 group doesnt supprt nsfw \n\n enable it by \n*${usedPrefix}enable* nsfw`
+    if (!global.db.data.chats[m.chat].nsfw) throw `🚫 This Group Doesnt Supprt nsfw \n\n Enable It By \n*${usedPrefix}enable* nsfw`
     let user = global.db.data.users[m.sender].age
-    if (user < 17) throw m.reply(`❎ uneed to be atleast 18 years`)
+    if (user < 17) throw m.reply(`❎ You Need To Be Atleast 18 Years`)
     if (!text) throw `*This command provides sauce from nhentai: ${usedPrefix + command} miku*`
     try {
     m.reply(global.wait)
