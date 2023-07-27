3addcb096017866457574df983b8b4cd62d2d9d9
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-27 23:30:49 +0530

    Update nsfw-nsfw.js

--- plugins/nsfw-nsfw.js
+++ plugins/nsfw-nsfw.js
@@ -2,9 +2,9 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 let handler = async (m, { conn, usedPrefix, command }) => {
 	
-	if (!global.db.data.chats[m.chat].nsfw) throw `🚫 group doesnt supprt nsfw \n\n enable it by \n*${usedPrefix}enable* nsfw`
+	if (!global.db.data.chats[m.chat].nsfw) throw `🚫 This Group Doesnt Supprt nsfw \n\n Enable It By \n*${usedPrefix}enable* nsfw`
     let user = global.db.data.users[m.sender].age
-    if (user < 17) throw m.reply(`❎ uneed to be atleast 18 years`) 
+    if (user < 17) throw m.reply(`❎ You Need To Be Atleast 18 Years`) 
    
 m.react(rwait)
 let type = (command).toLowerCase()
