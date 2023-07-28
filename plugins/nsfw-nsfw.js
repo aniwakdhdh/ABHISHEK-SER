aea1122adb810fbbadedd97e58bbbeb4fc27bac3
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-28 15:08:04 +0530

    Update nsfw-nsfw.js

--- plugins/nsfw-nsfw.js
+++ plugins/nsfw-nsfw.js
@@ -54,7 +54,7 @@ handler.help = ['ass', 'boobs', 'lesbian', 'pussy', 'pack']
 handler.tags = ['nsfw']
 handler.command = /^(ass|gand|boobs|boobies|lesbian|lesbians|pussy|chut|cosplay|pack)$/i
 handler.diamond = true
-handler.register = true
+handler.register = false
 handler.group = true
 
 export default handler
