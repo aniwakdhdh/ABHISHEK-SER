c44a6ec737c00fdd3f26b6a3641cb106e4181c17
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-28 13:58:51 +0530

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,7 +2,7 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `🎯 *Enter Serial Number*\nCheck Your Serial Number With The Command...\n\n*${usedPrefix}nserie*`
+  if (!args[0]) throw `🎯 *Enter Serial Number*\nCheck Your Serial Number With The Command...\n\n*${usedPrefix}sn*`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw '⚠️ *Incorrect Serial Number*'
