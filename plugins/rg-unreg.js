fb4140e0810b753681f0594ea4e1d6adffe01531
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-28 13:54:49 +0530

    Create rg-unreg.js

---
+++ plugins/rg-unreg.js
@@ -0,0 +1,18 @@
+//import db from '../lib/database.js'
+
+import { createHash } from 'crypto'
+let handler = async function (m, { conn, args, usedPrefix}) {
+  if (!args[0]) throw `🎯 *Enter Serial Number*\nCheck Your Serial Number With The Command...\n\n*${usedPrefix}nserie*`
+  let user = global.db.data.users[m.sender]
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  if (args[0] !== sn) throw '⚠️ *Incorrect Serial Number*'
+  user.registered = false
+  m.reply(`✅ Register Eliminated`)
+}
+handler.help = ['unreg <Num Serie>'] 
+handler.tags = ['rg']
+
+handler.command = ['unreg'] 
+handler.register = true
+
+export default handler
