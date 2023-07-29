890b0139f660dfc15323347bf3ab24e94a3fe25d
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-29 13:03:24 +0530

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -223,7 +223,7 @@ ${readMore}
 │   𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺 𝚂𝚄𝚁𝙴𝚂𝙷☘️
 ╰────────────⦁`  
 conn.sendFile(m.chat, pp, 'perfil.jpg', lkr, m, false, { mentions: [who] })
-m.react(done)
+m.react(mlc)
 }
 handler.help = ['allmenu']
 handler.tags = ['main']
