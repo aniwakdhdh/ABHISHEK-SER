3b38a166f331e6293af9b5da57e1c6dc77f56777
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-30 08:49:41 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -3,12 +3,12 @@ let handler = m => m
 handler.all = async function (m) {
 
     if (/^bot$/i.test(m.text) ) {
-      let av = 'https://imgur.io/EpRs8Lb'
+      let av = 'LINK HERE.mp3'
       this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
      }
 
-  if (/^buenas tardes$/i.test(m.text) ) {
-     let av = 'https://g.top4top.io/m_2437lm0y21.mp3'
+  if (/^test$/i.test(m.text) ) {
+     let av = 'https://i.imgur.com/0oCi5Lf.mp4'
      this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
@@ -17,6 +17,11 @@ handler.all = async function (m) {
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
   
+  if (/^buenas noches$/i.test(m.text) ) {
+    let av = 'https://e.top4top.io/m_2437afchn1.mp3'
+    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+   }
+    
 return !0
  }
  
