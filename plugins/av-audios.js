44fcaed32cd1d62ae95ce94e01135ded900a3d1e
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-30 21:07:22 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -3,7 +3,7 @@ let handler = m => m
 handler.all = async function (m) {
 
     if (/^.alive$/i.test(m.text) ) {
-      let av = 'https://s31.aconvert.com/convert/p3r68-cdx67/rcc9m-v7zgz.mp3'
+      let av = 'https://s19.aconvert.com/convert/p3r68-cdx67/e2oz9-ij972.mp3'
       this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
      }
 
