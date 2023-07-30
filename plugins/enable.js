ac696cc7d7a423109e3086b68b285b38947df2f6
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-30 19:56:10 +0530

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -160,7 +160,7 @@ const listMessage = {
         global.dfail('owner', m, conn)
         throw false
       }
-      bot.restrict = isEnable
+      bot.bgmbot = isEnable
       break
 		  
     case 'restrict':
