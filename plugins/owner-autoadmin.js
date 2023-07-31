b8eff612d80eacc9a9dbc4ffd0fcd50279c07874
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-01 02:07:16 +0530

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, isAdmin}) => {
   if (m.fromMe) return;
-  if (isAdmin) throw '*You Are Aldready Admin✅*';
+  if (isAdmin) throw '*You Are Aldready An Admin✅*';
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   } catch {
