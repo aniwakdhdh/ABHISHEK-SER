cc738d7976014dc93ffe0041f9da46b2a5e69eee
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-07-31 23:46:13 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -1,7 +1,7 @@
-p0let handler = m => m
+let handler = m => m
 handler.all = async function (m) {
 
-    if (/^.alive|bot$/i.test(m.text) ) {
+    if (/^.alive$/i.test(m.text) ) {
       let av = '../media/voice/Abhi.mp3'
       this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
      }
