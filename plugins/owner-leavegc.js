a9679cedc2be0e6bf432d40adcfde50c66b184af
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-01 08:24:13 +0530

    Create owner-leavegc.js

---
+++ plugins/owner-leavegc.js
@@ -0,0 +1,9 @@
+const handler = async (m, {conn, text, command}) => {
+  const id = text ? text : m.chat;
+  await conn.reply(id, '*GoodBye To All I M Leaving👋*');
+  await conn.groupLeave(id);
+};
+handler.command = /^left$/i;
+handler.group = true;
+handler.rowner = true;
+export default handler;
