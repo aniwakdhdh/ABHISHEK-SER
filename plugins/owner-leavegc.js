56802c9b85962bc6992f57a120f7897706230ddc
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-01 08:31:10 +0530

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, text, command}) => {
   const id = text ? text : m.chat;
-  await conn.reply(id, '*GoodBye To All I M Leaving👋*');
+  await conn.reply(id, '*GoodBye To All I M Lefting This Group👋*');
   await conn.groupLeave(id);
 };
 handler.command = /^left$/i;
