daacd3a9191654a479ed71079e0e11ecfbc7a117
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-03 00:55:24 +0530

    Create anonymou_chat.js.js

---
+++ plugins/anonymou_chat.js.js
@@ -0,0 +1,16 @@
+export async function before(m, {match}) {
+  // if (match) return !1
+  if (!m.chat.endsWith('@s.whatsapp.net')) {
+    return !0;
+  }
+  this.anonymous = this.anonymous ? this.anonymous : {};
+  const room = Object.values(this.anonymous).find((room) => [room.a, room.b].includes(m.sender) && room.state === 'CHATTING');
+  if (room) {
+    if (/^.*(next|leave|start)/.test(m.text)) {
+      return;
+    }
+    const other = [room.a, room.b].find((user) => user !== m.sender);
+    await m.copyNForward(other, true);
+  }
+  return !0;
+}
