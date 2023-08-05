353bdb28463e7fa2c8cd255d9a7a43d70aef91f2
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-05 18:54:29 +0530

    Update _TintuJoke.js

--- plugins/_TintuJoke.js
+++ plugins/_TintuJoke.js
@@ -1,1 +1,19 @@
-test
+const {Function,isPublic, getJson, getBuffer,prefix} = require("../lib/");
+Function({pattern: 'tintu ?(.*)', fromMe: isPublic, desc: 'tintumon jokes', type: 'fun'}, async (m, text, client) => {
+let tintus = ["https://i.ytimg.com/vi/p-BV4NbRv04/maxresdefault.jpg","https://i.ytimg.com/vi/AeGmfrRHtfw/mqdefault.jpg","https://yt3.ggpht.com/ytc/AKedOLQQzIv6c9zqrIHlabtdfXd2DeYIb2fA-9hmBJow=s900-c-k-c0x00ffffff-no-rj","https://i.ytimg.com/vi/FAh6o8tAans/maxresdefault.jpg","https://i1.ytimg.com/vi/YZSbzD5StgA/maxresdefault.jpg","https://i.ytimg.com/vi/m2iCiUrR4Wk/maxresdefault.jpg","https://i.ytimg.com/vi/53yuePI3L74/maxresdefault.jpg"]
+let image = tintus[Math.floor(Math.random()*tintus.length)]
+var { result } = await getJson("https://api-mask-ser.herokuapp.com/api/fun/tintu")
+
+const buttons = [
+  {buttonId: prefix + 'tintu', buttonText: {displayText: 'Next🎯'}, type: 1}]
+
+const buttonMessage = {
+    image: {url: image},
+    caption: '```' + result.joke + '```',
+    footer: 'ABHISHEK-SER',
+    buttons: buttons,
+    headerType: 4
+}
+
+client.sendMessage(m.jid, buttonMessage)
+});
