2ce31e71bfcf976e1bfa7c4fc02c817f68d4b820
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-06 16:05:03 +0530

    Update av-sticker.js

--- plugins/av-sticker.js
+++ plugins/av-sticker.js
@@ -3,17 +3,17 @@ handler.all = async function (m) {
 
     if (/^test$/i.test(m.text) ) {
       let av = 'https://i.ibb.co/4M7xYVx/30302726e59c.webp
-      this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'webp', ptt: true })
+      this.sendFile(m.chat, av, 'webp', null, m, true, { type: 'webp', ptt: true })
      }
 
   if (/^hi$/i.test(m.text) ) {
      let av = 'https://i.ibb.co/4M7xYVx/30302726e59c.webp'
-     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'webp', ptt: true })
+     this.sendFile(m.chat, av, 'webp', null, m, true, { type: 'webp', ptt: true })
    }
 
   if (/^cute$/i.test(m.text) ) {
     let av = 'https://i.ibb.co/4M7xYVx/30302726e59c.webp'
-    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'webp', ptt: true })
+    this.sendFile(m.chat, av, 'webp', null, m, true, { type: 'webp', ptt: true })
    }
   
 return !0
