d3600138438144ab7a7e1e32884291ed34ea1283
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-07 04:16:38 +0530

    Create random-gn.js

---
+++ plugins/random-gn.js
@@ -0,0 +1,19 @@
+
+let handler = async (m, { conn}) => {
+let user = global.db.data.users[m.sender]
+let name = conn.getName(m.sender)
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let av = `${pickRandom(["test", "Abhi", "Abhi", "Abhi", "hi"])}`
+
+m.reply( `👋Hello ${taguser} Good Night My Friend Sweet Dreams`)
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+} 
+
+handler.customPrefix = /^.gn$/i
+handler.command = new RegExp
+
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
