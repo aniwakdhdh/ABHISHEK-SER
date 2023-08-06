ea174bc78f95543417d05b098bef1c48a1635d95
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-06 16:17:03 +0530

    Update av-sticker.js

--- plugins/av-sticker.js
+++ plugins/av-sticker.js
@@ -3,7 +3,7 @@ handler.all = async function (m) {
 
     if (/^test$/i.test(m.text) ) {
       let av = 'https://i.ibb.co/4M7xYVx/30302726e59c.webp
-      this.sendFile(m.chat, av, 'webp', null, m, true, { type: 'webp', ptt: false })
+      this.sendFile(m.chat, av, 'webp', null, m, true, { type: 'webp',true })
      }
 
   if (/^hi$/i.test(m.text) ) {
