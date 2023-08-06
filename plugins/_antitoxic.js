5d77bcf915d330ddfb27517f3c06f54a49143260
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-06 23:09:08 +0530

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -14,12 +14,12 @@ export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
 
   if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
     user.warn += 1;
-    if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hey *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, The Word (${isToxic}) Is Prohibited In This Bot *${user.warn}/5* Warning`, false, {mentions: [m.sender]});
+    if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hey *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, The Word (${isToxic}) Is Prohibited In This Bot *${user.warn}/5* Warning⚠️`, false, {mentions: [m.sender]});
   }
 
   if (user.warn >= 5) {
     user.warn = 0;
-    await m.reply(`Hey *@${m.sender.split`@`[0]}*, You Reached The 5 Warnings You Will Be Blocked And Kicked Out From This Group`, false, {mentions: [m.sender]});
+    await m.reply(`Hey *@${m.sender.split`@`[0]}*, You Reached The 5 Warnings You Will Be Blocked And Kicked Out From This Group⚠️`, false, {mentions: [m.sender]});
     user.banned = true;
     await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
     // await this.updateBlockStatus(m.sender, 'block')
