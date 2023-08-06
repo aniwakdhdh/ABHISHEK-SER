befd54b97d87e070ba861ae0d877f93851a33a64
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-06 12:37:10 +0530

    Update dl-spotify.js

--- plugins/dl-spotify.js
+++ plugins/dl-spotify.js
@@ -1,8 +1,17 @@
 import fetch from 'node-fetch';
 import Spotify from 'spotifydl-x';
 import fs from 'fs';
-const handler = async (m, {conn, text}) => {
-  if (!text) throw `*⚠️Enter A Song Name*`;
+import NodeID3 from 'node-id3';
+import axios from 'axios';
+
+const credentials = {
+  clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3',
+  clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009',
+};
+const spotify = new Spotify.default(credentials);
+
+const handler = async (m, { conn, text }) => {
+ if (!text) throw `*⚠️Enter A Song Name *📌Example* Heat Waves.*`;
   try {
     const resDL = await fetch(`https://api.lolhuman.xyz/api/spotifysearch?apikey=${lolkeysapi}&query=${text}`);
     const jsonDL = await resDL.json();
@@ -13,30 +22,55 @@ const handler = async (m, {conn, text}) => {
     };
     const randomName = getRandom('.mp3');
     const filePath = `./tmp/${randomName}`;
-    fs.writeFileSync(filePath, spty.audio);
-    const spotifyi = `❒═════❬ 𝐒𝐏𝐎𝐓𝐈𝐅𝐘 ❭═════╾❒\n┬\n├‣✨ *TITLE:* ${spty.data.name}\n┴\n┬\n├‣🗣️ *ARTIST:* ${spty.data.artists}\n┴\n┬\n├‣🌐 *URL*: ${linkDL}\n┴`;
+    const artist = spty.data.artists.join(', ') || '-';
+    const tags = {
+      title: spty.data.name || '-',
+      artist: artist,
+      album: spty.data.album_name || '-',
+      year: spty.data.release_date || '-',
+      genre: '-',
+      comment: {
+        language: 'spa',
+        text: '🤴🏻 Descarga por BrunoSobrino & TheMystic-Bot-MD 🤖',
+      },
+      unsynchronisedLyrics: {
+        language: 'spa',
+        text: '🤴🏻 Descarga por BrunoSobrino & TheMystic-Bot-MD 🤖',
+      },
+      image: {
+        mime: 'image/jpeg',
+        type: {
+          id: 3,
+          name: 'front cover',
+        },
+        description: 'Spotify Thumbnail',
+        imageBuffer: await axios.get(spty.data.cover_url, { responseType: "arraybuffer" }).then((response) => Buffer.from(response.data, "binary")),
+      },
+      mimetype: 'image/jpeg',
+      copyright: 'Copyright Darlyn ©2023',
+    };
+    await fs.promises.writeFile(filePath, spty.audio);
+    await NodeID3.write(tags, filePath);
+    const spotifyi = `❒═════❬ 𝐒𝐏𝐎𝐓𝐈𝐅𝐘 ❭═════╾❒\n┬\n├‣✨ *TÍTULO:* ${spty.data.name}\n┴\n┬\n├‣🗣️ *ARTISTA:* ${spty.data.artists}\n┴\n┬\n├‣🌐 *𝚄𝚁𝙻*: ${linkDL}\n┴`;
     await conn.sendFile(m.chat, spty.data.cover_url, 'error.jpg', spotifyi, m);
-    await conn.sendMessage(m.chat, {audio: fs.readFileSync(`./tmp/${randomName}`), fileName: `${spty.data.name}.mp3`, mimetype: 'audio/mp4'}, {quoted: m});
-  } catch {
-    throw '**';
+    await conn.sendMessage(m.chat, { audio: fs.readFileSync(`./tmp/${randomName}`), fileName: `${spty.data.name}.mp3`, mimetype: 'audio/mp4' }, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    throw '*❌Downloading Failed.*';
   }
 };
-handler.command = /^(spotify|song)$/i;
+handler.command = /^(spotify|music)$/i;
 export default handler;
 
-const credentials = {clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3', clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009'};
-const spotify = new Spotify.default(credentials);
 async function spotifydl(url) {
   return new Promise(async (resolve, reject) => {
-    const res = await spotify.getTrack(url).catch(() => {
-      return { error: '*❌Downloading Failed*' };
-    });
-    const timeoutPromise = new Promise((_, reject) => {
-      setTimeout(() => {
-        reject(new Error('*⌛Timeout*'));
-      }, 300000); 
-    });
     try {
+      const res = await spotify.getTrack(url);
+      const timeoutPromise = new Promise((_, reject) => {
+        setTimeout(() => {
+          reject(new Error('⌛Timeout'));
+        }, 300000);
+      });
       const audioPromise = spotify.downloadTrack(url);
       const audio = await Promise.race([audioPromise, timeoutPromise]);
       resolve({ data: res, audio });
