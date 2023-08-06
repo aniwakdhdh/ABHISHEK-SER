572b067e2ada37f05968054ef398f10c7f4153c6
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-07 02:36:09 +0530

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -6,8 +6,8 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) =
 	var limit
      if((isOwner || isPrems)) limit = 1200
      else limit = 100
-   if (!args[0]) throw `✳️ Enter the mediafire link next to the command`
-    if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrect`
+   if (!args[0]) throw `*🎯 Enter The Mediafire link next to the command*`
+    if (!args[0].match(/mediafire/gi)) throw `*❌ Link Incorrect*`
     m.react(rwait)
     let full = /f$/i.test(command)
     let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
@@ -22,7 +22,7 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) =
 ▢ *Size:* ${filesizeH}
 ▢ *Extension:* ${ext}
 ▢ *Uploaded:* ${aploud}
-${isLimit ? `\n▢ The file exceeds the download limit *+${limit} MB*\nUpgrade to premium to be able to download files more than *900 MB*` : ''} 
+${isLimit ? `\n▢ The File Exceeds The Download Limit *+${limit} MB*\nUpgrade To Premium To Be Able To Download Giles More Than *900 MB*` : ''} 
 `.trim()
     await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
     
