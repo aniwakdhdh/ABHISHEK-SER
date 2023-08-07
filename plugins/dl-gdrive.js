0bbc1512364b5cd46ae253e6ab6c88d689cdaf57
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-07 13:44:13 +0530

    Update dl-gdrive.js

--- plugins/dl-gdrive.js
+++ plugins/dl-gdrive.js
@@ -2,7 +2,7 @@
 import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `✳️ Enter a Google Drive link`
+	if (!args[0]) throw `🎯 Enter A Google Drive Link`
 	m.react(rwait) 
 	try {
 	let res = await fg.GDriveDl(args[0])
@@ -16,13 +16,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
 	m.react(done)
    } catch {
-	m.reply('Error: Check the link or try another link') 
+	m.reply('Error: Check The Link Or Try Another Link') 
   }
 }
 handler.help = ['gdrive']
 handler.tags = ['dl', 'prem']
 handler.command = ['gdrive']
-handler.diamond = true
-handler.premium = true
+handler.diamond = false
+handler.premium = false
 
 export default handler
