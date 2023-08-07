6e6b24256077782672738276fff8c1bf39490771
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-07 14:39:31 +0530

    Update ABHI-hentai.js

--- plugins/ABHI-hentai.js
+++ plugins/ABHI-hentai.js
@@ -1,8 +1,8 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
 const handler = async (m, {conn, text, __dirname, usedPrefix, command}) => {
-  if (!global.db.data.chats[m.chat].nsfw && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 *';
-  if (!text) throw '*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝙷𝙴𝙽𝚃𝙰𝙸 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*';
+  if (!global.db.data.chats[m.chat].nsfw && m.isGroup) throw '🚫This Group Doesnt Supprt Nsfw \n\n Enable It By \n*${usedPrefix}on* nsfw';
+  if (!text) throw '⚠️Enter The Type Name *📌Example: ${usedPrefix}hentais hot*';
   const searchResults = await searchHentai(text);
   let teks = searchResults.result.map((v, i) => `
 ${i+1}. *_${v.title}_*
@@ -14,11 +14,11 @@ ${i+1}. *_${v.title}_*
     randomThumbnail = searchResults.result[randomIndex].thumbnail;
   } else {
     randomThumbnail = 'https://pictures.hentai-foundry.com/e/Error-Dot/577798/Error-Dot-577798-Zero_Two.png';
-    teks = '*[❗] 𝙽𝙾 𝚂𝙴 𝙷𝙰𝙽 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙰𝙳𝙾 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂*';
+    teks = '*❌No Results Found*';
   }
   conn.sendFile(m.chat, randomThumbnail, 'error.jpg', teks, m);
 };
-handler.command = /^(hentaisearch|hentai)$/i;
+handler.command = /^(hentaisearch|hentais)$/i;
 export default handler;
 async function searchHentai(search) {
   return new Promise((resolve, reject) => {
