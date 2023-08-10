cdea9420713a2833b1ab55d1ee4f49c965b11a32
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-10 13:19:54 +0530

    Update welgc_byegc.js

--- plugins/welgc_byegc.js
+++ plugins/welgc_byegc.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, text, usedPrefix, command, customPrefix }) => {
 let stikerwelgc = "./src/welgc.webp"
 let stikerbyegc = "./src/byegc.webp"
 if (command == 'welcomegc') {
-conn.sendFile(m.chat, stikerwelgc, 'sticker.webp', null, m, false, { 
+conn.sendFile(m.chat, stikerwelgc, 'sticker.webp', null, m, true, { 
 if (command == 'byegc') {
-conn.sendFile(m.chat, stikerbyegc, 'sticker.webp', null, m, false, { 
+conn.sendFile(m.chat, stikerbyegc, 'sticker.webp', null, m, true, { 
 }}
 handler.command = ['welcomegc', 'byegc']
 export default handler
