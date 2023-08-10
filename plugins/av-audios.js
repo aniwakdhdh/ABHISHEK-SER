1f7c312efe387a681cbf487e3fc3088a34e3d46c
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-10 12:42:39 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -2,17 +2,17 @@ let handler = m => m
 handler.all = async function (m) {
 
     if (/^.alive$/i.test(m.text) ) {
-      let av = '../media/voice/Abhi.mp3'
+      let av = '../src/mp3/Abhi.mp3'
       this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
      }
 
   if (/^.menu$/i.test(m.text) ) {
-     let av = '../media/voice/menu.mp3'
+     let av = '../src/mp3/menu.mp3'
      this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
   if (/^bot$/i.test(m.text) ) {
-    let av = 'https://raw.githubusercontent.com/AbhishekSuresh2/ABHISHEK-SER/main/media/bgm.json/bot'
+    let av = '../src/mp3/Abhi.mp3'
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
   
