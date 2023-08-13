6e3fb4afb4d7839d09fd6db701be2f9bb790105b
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-13 14:41:40 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -3,11 +3,13 @@ handler.all = async function (m) {
 
     if (/^.alive$/i.test(m.text) ) {
       let av = '../src/mp3/Abhi.mp3'
+      this.sendPresenceUpdate('recording', m.chat);
       this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
       }
 
   if (/^.menu$/i.test(m.text) ) {
     let av = '../src/mp3/menu.mp3'
+    this.sendPresenceUpdate('recording', m.chat);
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
@@ -25,7 +27,11 @@ handler.all = async function (m) {
     let av = '../src/mp3/love.mp3'
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
-    
+
+  if (/^bgm$/i.test(m.text) ) {
+    let av = '../src/mp3/bgm.mp3'
+    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+   }
     
   if (/^hi|hai|hey|hii$/i.test(m.text) ) {
     let av = '../src/mp3/hi.mp3'
