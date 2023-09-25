99c8d9c8536ec262c7ea22108139bfa05c05281c
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-09-25 21:53:55 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -13,28 +13,8 @@ handler.all = async function (m) {
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
-  if (/^happy|heppy|😁$/i.test(m.text) ) {
-    let av = './src/mp3/happy.mp3'
-    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-   }
-
-  if (/^sad|sed|🥺|😭|😢|😥$/i.test(m.text) ) {
-    let av = './src/mp3/sad.mp3'
-    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-   }
-
-  if (/^love|lub|❤️|i like you$/i.test(m.text) ) {
-    let av = './src/mp3/love.mp3'
-    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-   }
-
   if (/^bgm$/i.test(m.text) ) {
     let av = './src/mp3/bgm.mp3'
-    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-   }
-    
-  if (/^hi|hai|hey|hii$/i.test(m.text) ) {
-    let av = './src/mp3/hi.mp3'
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
   
