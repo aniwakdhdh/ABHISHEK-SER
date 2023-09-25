c7210553e0dbcd4c29130e9a607c141359a53e6e
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-09-26 00:11:28 +0530

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,15 +1,21 @@
-//import db from '../lib/database.js'
-
-import { execSync } from 'child_process'
-let handler = async (m, { conn, text }) => {
-if (conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-//require('fs').readdirSync('plugins').map(v=>global.reload('', v))
-conn.reply(m.chat, stdout.toString(), m)
-}}
+let handler = async (m, { conn, usedPrefix, command}) => {
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `❌ The User Is Not Found In My Database`
+let pp = 'https://i.ibb.co/jJGwL7b/f2803cd7f5f2.jpg'
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850) 
+let lkr = `╭───❮ *𝚄𝙿𝙳𝙰𝚃𝙴 𝙱𝙾𝚃* ❯
+│ _*ABHISHEK-SER BOT UPDATE🔧*_
+│ 1. Open Your Fork
+│ 2. Click On Sync Fork
+│ 3. And Click On Update Branch
+│ 4. After Redeploy The Bot
+╰─────────────⦁`  
+conn.sendFile(m.chat, pp, 'perfil.jpg', lkr, m, false, { mentions: [who] })
+m.react('🔧')
+}
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
-handler.rowner = true
+handler.command = ['update'] 
 
 export default handler
