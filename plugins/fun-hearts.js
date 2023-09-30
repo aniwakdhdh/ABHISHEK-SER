8a2c06e9ce13a6f9a6232f81949c79be9ae136d4
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-01 00:26:16 +0530

    Create fun-hearts.js

---
+++ plugins/fun-hearts.js
@@ -0,0 +1,37 @@
+import { exec } from 'child_process';
+import speed from 'performance-now';
+
+let handler = async (m, { conn }) => {
+  let heartMsg = await conn.sendMessage(m.chat, { text: '*Loading...*' });
+
+  let emojis = ["❤️", "🧡", "💛", "💖", "💚", "💓", "💙", "💜", "💝", "🖤", "🤎", "💕", "🤍", "💗", "_Made By ©ABHISHEK-SER_"];
+  let delay = 1000; // Delay in milliseconds between editing emojis
+
+  let timestamp = speed();
+
+  await exec('neofetch --stdout', async (error, stdout) => {
+    let latency = (speed() - timestamp).toFixed(4);
+
+    for (let emoji of emojis) {
+      setTimeout(async () => {
+        await conn.relayMessage(m.chat, {
+          protocolMessage: {
+            key: heartMsg.key,
+            type: 14,
+            editedMessage: {
+              conversation: emoji,
+            },
+          },
+        }, {});
+      }, delay);
+
+      delay += 1000; // Increase the delay for the next emoji
+    }
+  });
+};
+
+handler.help = ['hearts'];
+handler.tags = ['fun'];
+handler.command = ['hearts', 'heart'];
+
+export default handler;
