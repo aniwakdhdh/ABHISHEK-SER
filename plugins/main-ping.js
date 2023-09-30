b28d3194529f7c0cdb22d5e5d3639f541d2796f4
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-09-30 14:10:15 +0530

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -1,26 +1,32 @@
-import speed from 'performance-now';
-import { spawn, exec, execSync } from 'child_process';
+import { exec } from 'child_process'
+import speed from 'performance-now'
 
 let handler = async (m, { conn }) => {
-  // Send initial "Ping..." message
-  const pingMsg = await conn.sendMessage(m.chat, 'Ping...', 'text', { quoted: m });
 
-  let timestamp = speed();
-  let latensi = speed() - timestamp;
+  let pingMsg = await conn.sendMessage(m.chat, {text: '*ping*'})
 
-  exec(`neofetch --stdout`, async (error, stdout, stderr) => {
-    let child = stdout.toString("utf-8");
-    let ssd = child.replace(/Memory:/, "Ram:");
+  let timestamp = speed()
 
-    // Edit the initial "Ping..." message to "Pong" with latency
-    await conn.sendMessage(m.chat, `${ssd}🎯 *Pong*: ${latensi.toFixed(4)} *ms*`, 'text', {
-      quoted: pingMsg,
-    });
-  });
-};
+  await exec('neofetch --stdout', async (error, stdout) => {
 
-handler.help = ['ping'];
-handler.tags = ['main'];
-handler.command = ['ping', 'speed'];
+    let latency = (speed() - timestamp).toFixed(4)
 
-export default handler;
+    await conn.relayMessage(m.chat, {
+      protocolMessage: {
+        key: pingMsg.key,
+        type: 14,
+        editedMessage: {
+          conversation: `🎯pong! ${latency} ms` 
+        }
+      }
+    }, {})
+
+  })
+
+}
+
+handler.help = ['ping']
+handler.tags = ['main']
+handler.command = ['ping', 'speed'] 
+
+export default handler
