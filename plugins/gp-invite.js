baf29dd8f58f32d1f5cf6c26b2834a2aeabba3e6
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-17 03:13:17 +0530

    Update gp-invite.js

--- plugins/gp-invite.js
+++ plugins/gp-invite.js
@@ -1,16 +1,16 @@
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!text) throw `✳️ Enter the number you want to send a group invite to\n\n📌 Example :\n*${usedPrefix + command}* 7605902011`
-if (text.includes('+')) throw  `✳️ Ingrese el número todo junto sin el *+*`
-if (isNaN(text)) throw ' 📌 Enter only numbers plus your country code with no spaces'
+if (!text) throw `🎯 Enter The Number You Want To Send A Group Invite Link\n\n📌 Example :\n*${usedPrefix + command}* 919074692450`
+if (text.includes('+')) throw  `🎯 Ingrese el número todo junto sin el *+*`
+if (isNaN(text)) throw '*📌 Enter Only Numbers With Your Country Code With No Spaces*'
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
  
-      await conn.reply(text+'@s.whatsapp.net', `≡ *INVITATION TO GROUP*\n\nA user invited you to join this group \n\n${link}`, m, {mentions: [m.sender]})
-        m.reply(`✅ An invite link was sent to the user`) 
+      await conn.reply(text+'@s.whatsapp.net', `≡ *INVITATION TO GROUP*\n\nA User Invited You To Join This Group \n\n${link}`, m, {mentions: [m.sender]})
+        m.reply(`✅ An Invite Link Was Sent To The Sser`) 
 
 }
-handler.help = ['invite <917xxx>']
+handler.help = ['invite <919xxx>']
 handler.tags = ['group']
 handler.command = ['invite','invitar'] 
 handler.group = true
