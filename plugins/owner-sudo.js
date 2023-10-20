63a9b0f6e16862bc20566eeecd1f07ed217f5978
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-20 09:22:39 +0530

    Update owner-sudo.js

--- plugins/owner-sudo.js
+++ plugins/owner-sudo.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, text }) => {
 }
 handler.help = ['sudo @user']
 handler.tags = ['owner']
-handler.command = /^(owner|sudo)$/i;
+handler.command = /^sudo/i;
 handler.owner = true
 
 export default handler;
